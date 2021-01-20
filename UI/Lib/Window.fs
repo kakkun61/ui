0becd274f240af70777bc6daa9cfe7bc9618ae37
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2021-01-20 09:27:24 +0900

    F# Window

---
+++ UI/Lib/Window.fs
@@ -0,0 +1,19 @@
+﻿namespace Hui
+
+open Microsoft.UI.Xaml.Controls
+
+type private UiWindow = Microsoft.UI.Xaml.Window
+type private HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment
+type private VerticalAlignment = Microsoft.UI.Xaml.VerticalAlignment
+
+type Window() as self =
+    inherit UiWindow()
+
+    let button = new Button(Content = "Click me")
+    let panel = new StackPanel(Orientation = Orientation.Horizontal,
+                               HorizontalAlignment = HorizontalAlignment.Center,
+                               VerticalAlignment = VerticalAlignment.Center)
+
+    do
+        self.Content <- panel
+        panel.Children.Add(button)
--- UI/UI/App.xaml.cs
+++ UI/UI/App.xaml.cs
@@ -43,7 +43,7 @@ namespace UI
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
+            m_window = new Hui.Window();
             m_window.Activate();
         }
 
